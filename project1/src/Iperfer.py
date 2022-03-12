c25ccc456ca95cc1f7de8c1ae126f401a193cf48
AlexYuser <92759371+AlexYuser@users.noreply.github.com>
2022-03-11 23:34:27 -0500

    Update Iperfer.py

--- project1/src/Iperfer.py
+++ project1/src/Iperfer.py
@@ -28,7 +28,7 @@ counter=0
 
 timer=time.perf_counter()
 
-while timer - time.imputTime() < imputTime:
+while timer - time.perf_counter() < imputTime:
     clientSocket.send(dataSent)
     counter+=1
     continue
